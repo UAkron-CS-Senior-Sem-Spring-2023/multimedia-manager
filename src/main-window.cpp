14eef073cc8ba01357a1a110ee5f002c8b975609
Gregory Robertson <robertson.gregory.a@gmail.com>
2023-02-24 13:38:17 -0500

    Remove many unnecessary unique_ptr's

--- src/main-window.cpp
+++ src/main-window.cpp
@@ -3,144 +3,128 @@
 #include <QPalette>
 
 MainWindow::MainWindow(QWidget* parent)
-    : QMainWindow(parent)
+    : QMainWindow(parent),
+    windowGrid(QGridLayout(&windowWidget)),
+    // construct manageable widget
+    manageableGrid(QGridLayout(&manageableWidget)),
+    managingAccountsLabel(QLabel(tr("Managed Accounts"))),
+    managingAccountGroupsLabel(QLabel(tr("Managed Account Groups"))),
+    manageableNamesLayout(QVBoxLayout(&manageableNamesWidget)),
+    addManageableAccountButton(QPushButton(tr("Add Account"))),
+    removeManageableAccountButton(QPushButton(tr("Remove Account"))),
+    modifyManageableAccountGroupLayout(QHBoxLayout(&modifyManageableAccountGroupWidget)),
+    addManageableAccountGroupButton(QPushButton(tr("Add Account Group"))),
+    removeManageableAccountGroupButton(QPushButton(tr("Remove Account Group"))),
+    modifyManageableAccountLayout(QHBoxLayout(&modifyManageableAccountWidget)),
+    manageableAccountsButton(QPushButton(tr("Managed Accounts"))),
+    manageableAccountGroupsButton(QPushButton(tr("Managed Account Groups"))),
+    manageableTypeGroupBox(QGroupBox(tr("Current Managed Type"))),
+    // construct managing widget
+    managingGrid(QGridLayout(&managingWidget)),
+    managingAccountsInfoLabel(QLabel(tr("Account Information"))),
+    managingAccountGroupsInfoLabel(QLabel(tr("Account Group Information"))),
+    managingInfoLayout(QVBoxLayout(&managingInfoWidget)),
+    managingSendMessageButton(QPushButton(tr("Send message"))),
+    managingViewInboxButton(QPushButton(tr("View inbox")))
 {
-    windowWidget = std::make_unique<QWidget>();
-    windowGrid = std::make_unique<QGridLayout>(windowWidget.get());
+    layoutManageableWidget();
+    windowGrid.addWidget(&manageableWidget, 0, 0);
 
-    constructManageableWidget();
-    windowGrid->addWidget(manageableWidget.get(), 0, 0);
+    layoutManagingWidget();
+    windowGrid.addWidget(&managingWidget, 0, 1);
 
-    constructManagingWidget();
-    windowGrid->addWidget(managingWidget.get(), 0, 1);
-
-    setCentralWidget(windowWidget.get());
+    setCentralWidget(&windowWidget);
 }
 
 void onClickManageableAccountsButton() {
 
 }
 
-void MainWindow::constructManageableWidget() {
+void MainWindow::layoutManageableWidget() {
     int workingRow = 0;
-    manageableWidget = std::make_unique<QWidget>();
 
-    manageableGrid = std::make_unique<QGridLayout>(manageableWidget.get());
-    manageableGrid->setColumnStretch(1, 1);
+    manageableGrid.setColumnStretch(1, 1);
 
-    managingTypeStackedLayout = std::make_unique<QStackedLayout>();
-    managingAccountsLabel = std::make_unique<QLabel>(tr("Managed Accounts"));
-    managingTypeStackedLayout->insertWidget(ACCOUNTS, managingAccountsLabel.get());
-    managingAccountGroupsLabel = std::make_unique<QLabel>(tr("Managed Account Groups"));
-    managingTypeStackedLayout->insertWidget(ACCOUNT_GROUPS, managingAccountGroupsLabel.get());
+    managingTypeStackedLayout.insertWidget(ACCOUNTS, &managingAccountsLabel);
+    managingTypeStackedLayout.insertWidget(ACCOUNT_GROUPS, &managingAccountGroupsLabel);
     
-    managingTypeStackedLayout->setCurrentIndex(managingType);
-    manageableGrid->addLayout(managingTypeStackedLayout.get(), workingRow, 0);
+    managingTypeStackedLayout.setCurrentIndex(managingType);
+    manageableGrid.addLayout(&managingTypeStackedLayout, workingRow, 0);
     ++workingRow;
 
     // set up manageable list
-    manageableNamesWidget = std::make_unique<QWidget>();
-    manageableNamesWidget->setBackgroundRole(QPalette::Base);
-    manageableNamesWidget->setAutoFillBackground(true);
-    manageableNamesLayout = std::make_unique<QVBoxLayout>(manageableNamesWidget.get());
-    manageableNamesLayout->setContentsMargins(0, 0, 0, 16);
+    manageableNamesWidget.setBackgroundRole(QPalette::Base);
+    manageableNamesWidget.setAutoFillBackground(true);
+    manageableNamesLayout.setContentsMargins(0, 0, 0, 16);
 
-    manageableGrid->setRowStretch(workingRow, 1);
-    manageableGrid->addWidget(manageableNamesWidget.get(), workingRow, 0, 1, 2);
+    manageableGrid.setRowStretch(workingRow, 1);
+    manageableGrid.addWidget(&manageableNamesWidget, workingRow, 0, 1, 2);
     ++workingRow;
 
     // set up buttons to modify accounts
-    modifyManageablesButtonsStackedLayout = std::make_unique<QStackedLayout>();
+    modifyManageableAccountLayout.addWidget(&addManageableAccountButton);
+    modifyManageableAccountLayout.addStretch(1);
+    modifyManageableAccountLayout.addWidget(&removeManageableAccountButton);
+    modifyManageablesButtonsStackedLayout.insertWidget(ACCOUNTS, &modifyManageableAccountWidget);
 
-    modifyManageableAccountWidget = std::make_unique<QWidget>();
-    addManageableAccountButton = std::make_unique<QPushButton>(tr("Add Account"));
-    removeManageableAccountButton = std::make_unique<QPushButton>(tr("Remove Account"));
-    modifyManageableAccountLayout = std::make_unique<QHBoxLayout>(modifyManageableAccountWidget.get());
-    modifyManageableAccountLayout->addWidget(addManageableAccountButton.get());
-    modifyManageableAccountLayout->addStretch(1);
-    modifyManageableAccountLayout->addWidget(removeManageableAccountButton.get());
-    modifyManageablesButtonsStackedLayout->insertWidget(ACCOUNTS, modifyManageableAccountWidget.get());
+    modifyManageableAccountGroupLayout.addWidget(&addManageableAccountGroupButton);
+    modifyManageableAccountGroupLayout.addStretch(1);
+    modifyManageableAccountGroupLayout.addWidget(&removeManageableAccountGroupButton);
+    modifyManageablesButtonsStackedLayout.insertWidget(ACCOUNT_GROUPS, &modifyManageableAccountGroupWidget);
 
-    modifyManageableAccountGroupWidget = std::make_unique<QWidget>();
-    addManageableAccountGroupButton = std::make_unique<QPushButton>(tr("Add Account Group"));
-    removeManageableAccountGroupButton = std::make_unique<QPushButton>(tr("Remove Account Group"));
-    modifyManageableAccountGroupLayout = std::make_unique<QHBoxLayout>(modifyManageableAccountGroupWidget.get());
-    modifyManageableAccountGroupLayout->addWidget(addManageableAccountGroupButton.get());
-    modifyManageableAccountGroupLayout->addStretch(1);
-    modifyManageableAccountGroupLayout->addWidget(removeManageableAccountGroupButton.get());
-    modifyManageablesButtonsStackedLayout->insertWidget(ACCOUNT_GROUPS, modifyManageableAccountGroupWidget.get());
-
-    modifyManageablesButtonsStackedLayout->setCurrentIndex(managingType);
-    manageableGrid->addLayout(modifyManageablesButtonsStackedLayout.get(), workingRow, 0, 1, 2);
+    modifyManageablesButtonsStackedLayout.setCurrentIndex(managingType);
+    manageableGrid.addLayout(&modifyManageablesButtonsStackedLayout, workingRow, 0, 1, 2);
     ++workingRow;
 
     // set up manageable account type buttons
-    manageableAccountsButton = std::make_unique<QPushButton>(tr("Managed Accounts"));
-    QObject::connect(manageableAccountsButton.get(), &QPushButton::clicked, this, &MainWindow::manageableAccountsButtonClicked);
-    manageableAccountGroupsButton = std::make_unique<QPushButton>(tr("Managed Account Groups"));
-    QObject::connect(manageableAccountGroupsButton.get(), &QPushButton::clicked, this, &MainWindow::manageableAccountGroupsButtonClicked);
-    
-    manageableTypeLayout = std::make_unique<QHBoxLayout>();
-    manageableTypeLayout->addWidget(manageableAccountsButton.get());
-    manageableTypeLayout->addWidget(manageableAccountGroupsButton.get());
-    manageableTypeLayout->addStretch(1);
-    
-    manageableTypeGroupBox = std::make_unique<QGroupBox>(tr("Current Managed Type"));
-    manageableTypeGroupBox->setLayout(manageableTypeLayout.get());
+    QObject::connect(&manageableAccountsButton, &QPushButton::clicked, this, &MainWindow::manageableAccountsButtonClicked);
+    QObject::connect(&manageableAccountGroupsButton, &QPushButton::clicked, this, &MainWindow::manageableAccountGroupsButtonClicked);
+    manageableTypeLayout.addWidget(&manageableAccountsButton);
+    manageableTypeLayout.addWidget(&manageableAccountGroupsButton);
+    manageableTypeLayout.addStretch(1);
+    manageableTypeGroupBox.setLayout(&manageableTypeLayout);
 
-    manageableGrid->addWidget(manageableTypeGroupBox.get(), workingRow, 0);
+    manageableGrid.addWidget(&manageableTypeGroupBox, workingRow, 0);
     ++workingRow;
 }
 
-void MainWindow::constructManagingWidget() {
+void MainWindow::layoutManagingWidget() {
     int workingRow = 0;
-    managingWidget = std::make_unique<QWidget>();
-
-    managingGrid = std::make_unique<QGridLayout>(managingWidget.get());
 
     // set up managed accounts info labels
-    managingTypeInfoStackedLayout = std::make_unique<QStackedLayout>();
-    managingAccountsInfoLabel = std::make_unique<QLabel>(tr("Account Information"));
-    managingTypeInfoStackedLayout->insertWidget(ACCOUNTS, managingAccountsInfoLabel.get());
-    managingAccountGroupsInfoLabel = std::make_unique<QLabel>(tr("Account Group Information"));
-    managingTypeInfoStackedLayout->insertWidget(ACCOUNT_GROUPS, managingAccountGroupsInfoLabel.get());
-    
-    managingTypeInfoStackedLayout->setCurrentIndex(managingType);
-    managingGrid->addLayout(managingTypeInfoStackedLayout.get(), workingRow, 0);
+    managingTypeInfoStackedLayout.insertWidget(ACCOUNTS, &managingAccountsInfoLabel);
+    managingTypeInfoStackedLayout.insertWidget(ACCOUNT_GROUPS, &managingAccountGroupsInfoLabel);
+    managingTypeInfoStackedLayout.setCurrentIndex(managingType);
+    managingGrid.addLayout(&managingTypeInfoStackedLayout, workingRow, 0);
     ++workingRow;
 
     // set up managed accounts info
-    managingInfoWidget = std::make_unique<QWidget>();
-    managingInfoWidget->setBackgroundRole(QPalette::Base);
-    managingInfoWidget->setAutoFillBackground(true);
-    managingInfoLayout = std::make_unique<QVBoxLayout>(managingInfoWidget.get());
-    managingInfoLayout->setContentsMargins(0, 0, 0, 16);
+    managingInfoWidget.setBackgroundRole(QPalette::Base);
+    managingInfoWidget.setAutoFillBackground(true);
+    managingInfoLayout.setContentsMargins(0, 0, 0, 16);
 
-    managingGrid->setRowStretch(workingRow, 1);
-    managingGrid->addWidget(managingInfoWidget.get(), workingRow, 0);
+    managingGrid.setRowStretch(workingRow, 1);
+    managingGrid.addWidget(&managingInfoWidget, workingRow, 0);
     ++workingRow;
 
     // set up actionable buttons for that account
-    managingSendMessageButton = std::make_unique<QPushButton>(tr("Send message"));
-    managingViewInboxButton = std::make_unique<QPushButton>(tr("View inbox"));
 
-    managingActionButtonsLayout = std::make_unique<QHBoxLayout>();
-    managingActionButtonsLayout->addWidget(managingSendMessageButton.get());
-    managingActionButtonsLayout->addStretch(1);
-    managingActionButtonsLayout->addWidget(managingViewInboxButton.get());
+    managingActionButtonsLayout.addWidget(&managingSendMessageButton);
+    managingActionButtonsLayout.addStretch(1);
+    managingActionButtonsLayout.addWidget(&managingViewInboxButton);
     // 66px magic number, gets buttons to align with lefthand side's textbox
-    managingActionButtonsLayout->setContentsMargins(0, 0, 0, 67);
+    managingActionButtonsLayout.setContentsMargins(0, 0, 0, 67);
 
-    managingGrid->addLayout(managingActionButtonsLayout.get(), workingRow, 0);
+    managingGrid.addLayout(&managingActionButtonsLayout, workingRow, 0);
     ++workingRow;
 }
 
 void MainWindow::updateManagingType(ManagingType type) {
     managingType = type;
 
-    managingTypeStackedLayout->setCurrentIndex(managingType);
-    managingTypeInfoStackedLayout->setCurrentIndex(managingType);
-    modifyManageablesButtonsStackedLayout->setCurrentIndex(managingType);
+    managingTypeStackedLayout.setCurrentIndex(managingType);
+    managingTypeInfoStackedLayout.setCurrentIndex(managingType);
+    modifyManageablesButtonsStackedLayout.setCurrentIndex(managingType);
 }
 
 void MainWindow::manageableAccountsButtonClicked() {
--- src/main-window.hpp
+++ src/main-window.hpp
@@ -24,59 +24,59 @@ class MainWindow : public QMainWindow {
         };
         ManagingType managingType = ACCOUNTS;
 
-        std::unique_ptr<QWidget> windowWidget;
-        std::unique_ptr<QGridLayout> windowGrid;
+        QWidget windowWidget;
+        QGridLayout windowGrid;
 
         // lefthand side of application
         // contains list of groups of accounts/accounts and buttons to switch management style
-        void constructManageableWidget();
-        std::unique_ptr<QWidget> manageableWidget;
-        std::unique_ptr<QGridLayout> manageableGrid;
+        void layoutManageableWidget();
+        QWidget manageableWidget;
+        QGridLayout manageableGrid;
 
-        std::unique_ptr<QStackedLayout> managingTypeStackedLayout;
-        std::unique_ptr<QLabel> managingAccountsLabel;
-        std::unique_ptr<QLabel> managingAccountGroupsLabel;
+        QStackedLayout managingTypeStackedLayout;
+        QLabel managingAccountsLabel;
+        QLabel managingAccountGroupsLabel;
 
-        std::unique_ptr<QWidget> manageableNamesWidget;
-        std::unique_ptr<QVBoxLayout> manageableNamesLayout;
-        std::vector<std::unique_ptr<QLabel>> manageableNames;
+        QWidget manageableNamesWidget;
+        QVBoxLayout manageableNamesLayout;
+        QLabel manageableNames;
 
-        std::unique_ptr<QStackedLayout> modifyManageablesButtonsStackedLayout;
+        QStackedLayout modifyManageablesButtonsStackedLayout;
 
-        std::unique_ptr<QWidget> modifyManageableAccountWidget;
-        std::unique_ptr<QHBoxLayout> modifyManageableAccountLayout;
-        std::unique_ptr<QPushButton> addManageableAccountButton;
-        std::unique_ptr<QPushButton> removeManageableAccountButton;
+        QWidget modifyManageableAccountWidget;
+        QHBoxLayout modifyManageableAccountLayout;
+        QPushButton addManageableAccountButton;
+        QPushButton removeManageableAccountButton;
 
-        std::unique_ptr<QWidget> modifyManageableAccountGroupWidget;
-        std::unique_ptr<QHBoxLayout> modifyManageableAccountGroupLayout;
-        std::unique_ptr<QPushButton> addManageableAccountGroupButton;
-        std::unique_ptr<QPushButton> removeManageableAccountGroupButton;
+        QWidget modifyManageableAccountGroupWidget;
+        QHBoxLayout modifyManageableAccountGroupLayout;
+        QPushButton addManageableAccountGroupButton;
+        QPushButton removeManageableAccountGroupButton;
 
-        std::unique_ptr<QGroupBox> manageableTypeGroupBox;
-        std::unique_ptr<QHBoxLayout> manageableTypeLayout;
+        QGroupBox manageableTypeGroupBox;
+        QHBoxLayout manageableTypeLayout;
         void manageableAccountsButtonClicked();
-        std::unique_ptr<QPushButton> manageableAccountsButton;
+        QPushButton manageableAccountsButton;
         void manageableAccountGroupsButtonClicked();
-        std::unique_ptr<QPushButton> manageableAccountGroupsButton;
+        QPushButton manageableAccountGroupsButton;
 
         // righthand side of application
         // contains the selected group of accounts/account and buttons to perform actions on that group/account
-        void constructManagingWidget();
-        std::unique_ptr<QWidget> managingWidget;
-        std::unique_ptr<QGridLayout> managingGrid;
+        void layoutManagingWidget();
+        QWidget managingWidget;
+        QGridLayout managingGrid;
 
-        std::unique_ptr<QStackedLayout> managingTypeInfoStackedLayout;
-        std::unique_ptr<QLabel> managingAccountsInfoLabel;
-        std::unique_ptr<QLabel> managingAccountGroupsInfoLabel;
+        QStackedLayout managingTypeInfoStackedLayout;
+        QLabel managingAccountsInfoLabel;
+        QLabel managingAccountGroupsInfoLabel;
 
-        std::unique_ptr<QWidget> managingInfoWidget;
-        std::unique_ptr<QVBoxLayout> managingInfoLayout;
-        std::vector<std::unique_ptr<QLabel>> managingInfoLabels;
+        QWidget managingInfoWidget;
+        QVBoxLayout managingInfoLayout;
+        std::vector<QLabel> managingInfoLabels;
 
-        std::unique_ptr<QHBoxLayout> managingActionButtonsLayout;
-        std::unique_ptr<QPushButton> managingSendMessageButton;
-        std::unique_ptr<QPushButton> managingViewInboxButton;
+        QHBoxLayout managingActionButtonsLayout;
+        QPushButton managingSendMessageButton;
+        QPushButton managingViewInboxButton;
 
         void updateManagingType(ManagingType type);
 };
