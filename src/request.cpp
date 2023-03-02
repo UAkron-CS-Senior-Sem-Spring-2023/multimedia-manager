705d1d535e5ac4a34b5b3809442e2095cfc1d071
Gregory Robertson <robertson.gregory.a@gmail.com>
2023-03-02 01:17:36 -0500

    Simplify request's maintaining of singleton status

--- src/request.cpp
+++ src/request.cpp
@@ -2,31 +2,34 @@
 
 #include <curl/curl.h>
 
-ImplRequest::ImplRequest() {
+Request::Request() {
     curl_global_init(CURL_GLOBAL_ALL);
     handle = curl_easy_init();
 }
 
-ImplRequest& ImplRequest::singleton() {
-    if (!singleton_) {
-        singleton_ = std::unique_ptr<ImplRequest>(new ImplRequest);
+std::unique_ptr<Request> requestSingleton_ = nullptr;
+Request& Request::singleton() {
+    if (!requestSingleton_) {
+        requestSingleton_ = std::unique_ptr<Request>(new Request);
     }
 
-    return *singleton_;
+    return *requestSingleton_;
 }
 
-std::unique_ptr<ImplRequest> ImplRequest::singleton_ = nullptr;
-
-std::size_t ImplRequest::handleData(void* buffer, std::size_t size, std::size_t bufferLength, void* userData) {
+std::size_t Request::handleData(void* buffer, std::size_t size, std::size_t bufferLength, void* userData) {
     data.append(static_cast<const char*>(buffer), bufferLength);
     return bufferLength;
 }
 
-std::size_t ImplRequest::handleDataStatic(void* buffer, std::size_t size, std::size_t bufferLength, void* userData) {
-    return ImplRequest::singleton().handleData(buffer, size, bufferLength, userData);
+std::size_t Request::handleDataStatic(void* buffer, std::size_t size, std::size_t bufferLength, void* userData) {
+    return Request::singleton().handleData(buffer, size, bufferLength, userData);
 }
 
-std::optional<std::string> ImplRequest::get(const std::string& url) {
+std::optional<std::string> Request::get(const std::string& url) {
+    return singleton().getImpl(url);
+}
+
+std::optional<std::string> Request::getImpl(const std::string& url) {
     data.clear();
     curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
     curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, handleDataStatic);
@@ -38,7 +41,11 @@ std::optional<std::string> ImplRequest::get(const std::string& url) {
     }
 }
 
-std::optional<std::string> ImplRequest::post(const std::string& url, const std::unordered_map<std::string, std::string>& postFields) {
+std::optional<std::string> Request::post(const std::string& url, const std::unordered_map<std::string, std::string>& postFields) {
+    return singleton().postImpl(url, postFields);
+}
+
+std::optional<std::string> Request::postImpl(const std::string& url, const std::unordered_map<std::string, std::string>& postFields) {
     data.clear();
     curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
     curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, handleDataStatic);
@@ -61,12 +68,4 @@ std::optional<std::string> ImplRequest::post(const std::string& url, const std::
     } else {
         return std::optional<std::string>(std::string(std::move(data)));
     }
-}
-
-std::optional<std::string> Request::get(const std::string& url) {
-    return ImplRequest::singleton().get(url);
-}
-
-std::optional<std::string> Request::post(const std::string& url, const std::unordered_map<std::string, std::string>& postFields) {
-    return ImplRequest::singleton().post(url, postFields);
 }
--- src/request.hpp
+++ src/request.hpp
@@ -7,24 +7,19 @@
 
 #include <curl/curl.h>
 
-class ImplRequest {
+class Request {
     public:
-        static ImplRequest& singleton();
-
-        std::optional<std::string> get(const std::string& url);
-        std::optional<std::string> post(const std::string& url, const std::unordered_map<std::string, std::string>& postFields);
+        static std::optional<std::string> get(const std::string& url);
+        static std::optional<std::string> post(const std::string& url, const std::unordered_map<std::string, std::string>& postFields);
     private:
+        static Request& singleton();
+        std::optional<std::string> getImpl(const std::string& url);
+        std::optional<std::string> postImpl(const std::string& url, const std::unordered_map<std::string, std::string>& postFields);
 
         CURL* handle;
         std::string data;
         std::size_t handleData(void* buffer, std::size_t size, std::size_t bufferLength, void* userData);
         static std::size_t handleDataStatic(void* buffer, std::size_t size, std::size_t bufferLength, void* userData);
 
-        ImplRequest();
-        static std::unique_ptr<ImplRequest> singleton_;
-};
-
-namespace Request {
-    std::optional<std::string> get(const std::string& url);
-    std::optional<std::string> post(const std::string& url, const std::unordered_map<std::string, std::string>& postFields);
+        Request();
 };
